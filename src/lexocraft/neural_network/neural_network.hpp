ad2a6b8f9723d8071c4be7da1781460405f22ddc
FuexFollets <fuex9follets@gmail.com>
2023-12-28 13:59:47 -0500

    Added nerual network computational functions and NeuralNetworkDiff subclass

--- src/lexocraft/neural_network/neural_network.hpp
+++ src/lexocraft/neural_network/neural_network.hpp
@@ -23,6 +23,32 @@ namespace lc {
         explicit NeuralNetwork(std::vector<std::uint8_t> alpaca_bytes);
 
         [[nodiscard]] std::vector<std::uint8_t> serialize() const;
+        [[nodiscard]] Eigen::VectorXf compute(Eigen::VectorXf input) const;
+
+        class NeuralNetworkDiff {
+            std::vector<Eigen::MatrixXf> _weight_diffs;
+            std::vector<Eigen::VectorXf> _bias_diffs;
+            std::vector<std::size_t> _layer_sizes;
+
+            public:
+
+            explicit NeuralNetworkDiff(std::vector<std::size_t> layer_sizes);
+
+            NeuralNetworkDiff operator+(const NeuralNetworkDiff& other) const;
+            NeuralNetworkDiff operator-(const NeuralNetworkDiff& other) const;
+            NeuralNetworkDiff operator*(float scalar) const;
+            NeuralNetworkDiff operator/(float scalar) const;
+            NeuralNetworkDiff& operator+=(const NeuralNetworkDiff& other);
+            NeuralNetworkDiff& operator-=(const NeuralNetworkDiff& other);
+            NeuralNetworkDiff& operator*=(float scalar);
+            NeuralNetworkDiff& operator/=(float scalar);
+
+            friend class NeuralNetwork;
+        };
+
+        void modify(NeuralNetworkDiff diff);
+
+        static float sigmoid(float value);
     };
 
 } // namespace lc
