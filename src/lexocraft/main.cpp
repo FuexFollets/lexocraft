41760b429ec5e42adb2615e764749af8495db8e7
FuexFollets <fuex9follets@gmail.com>
2023-12-28 14:04:25 -0500

    Fixed compile errors in main and test funciton

--- src/lexocraft/main.cpp
+++ src/lexocraft/main.cpp
@@ -1,15 +1,7 @@
-#include <Eigen/Dense>
 #include <iostream>
-#include <lexocraft/neural_network/nn.hpp>
 
 int main() {
-    Eigen::MatrixXd matrix(3, 3);
-    matrix << 1, 2, 3, 4, 5, 6, 7, 8, 9;
-
-    std::cout << "Calling function " << nnfunction(100);
-
-    // Your code using Eigen
-    std::cout << matrix << '\n';
+    std::cout << "Hello, World!\n";
 
     return 0;
 }
--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
-#include <lexocraft/neural_network/nn.hpp>
 
 int main() {
-    auto output {nnfunction(100)};
-
-    std::cout << "hello world\n"
-              << "Your output is: " << output << "\n";
+    std::cout << "hello world\n";
 
     return 0;
 }
