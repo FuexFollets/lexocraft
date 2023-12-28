8ac336a09e05bca7f5f51ecff48aaf7a1223bfdb
FuexFollets <fuex9follets@gmail.com>
2023-12-28 13:34:52 -0500

    Edited neural network header

--- src/lexocraft/neural_network/neural_network.hpp
+++ src/lexocraft/neural_network/neural_network.hpp
@@ -2,6 +2,8 @@
 #define LEXICRAFT_NEURAL_NETWORK_HPP
 
 #include <cstddef>
+#include <vector>
+
 #include <eigen3/Eigen/Core>
 
 namespace lc {
@@ -12,18 +14,17 @@ namespace lc {
         std::size_t _iterations;
         std::vector<std::size_t> _layer_sizes;
 
-        std::vector<Eigen::MatrixXd> _weights;
-        std::vector<Eigen::VectorXd> _biases;
+        std::vector<Eigen::MatrixXf> _weights;
+        std::vector<Eigen::VectorXf> _biases;
 
         public:
 
         explicit NeuralNetwork(std::vector<std::size_t> layer_sizes, bool randomize = true);
         explicit NeuralNetwork(std::vector<std::uint8_t> alpaca_bytes);
 
-        [[nodiscard]] std::vector<std::uint8_t> to_bytes() const;
+        [[nodiscard]] std::vector<std::uint8_t> serialize() const;
     };
 
 } // namespace lc
-
 
 #endif // LEXICRAFT_NEURAL_NETWORK_HPP
