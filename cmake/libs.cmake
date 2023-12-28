8771a1940d9b58fddcb909922990b58d8b163d56
FuexFollets <fuex9follets@gmail.com>
2023-12-28 11:39:57 -0500

    Added cmake files for libraries and options

---
+++ cmake/libs.cmake
@@ -0,0 +1,7 @@
+include_directories(${PROJECT_SOURCE_DIR}/lib/eigen)
+include_directories(${PROJECT_SOURCE_DIR}/src)
+
+set(LEXICRAFT_LIBS neural_network)
+
+# include(${PROJECT_SOURCE_DIR}/cmake/libs.cmake)
+# include(${PROJECT_SOURCE_DIR}/cmake/opts.cmake)
