7f581543d85aa166af1f66c9f57b6df6630940cf
FuexFollets <fuex9follets@gmail.com>
2023-12-29 13:12:16 -0500

    Added additional getter methods and training method

--- src/lexocraft/neural_network/neural_network.hpp
+++ src/lexocraft/neural_network/neural_network.hpp
@@ -9,21 +9,7 @@
 namespace lc {
 
     class NeuralNetwork {
-        private:
-
-        std::size_t _iterations;
-        std::vector<std::size_t> _layer_sizes;
-
-        std::vector<Eigen::MatrixXf> _weights;
-        std::vector<Eigen::VectorXf> _biases;
-
         public:
-
-        explicit NeuralNetwork(std::vector<std::size_t> layer_sizes, bool randomize = true);
-        explicit NeuralNetwork(std::vector<std::uint8_t> alpaca_bytes);
-
-        [[nodiscard]] std::vector<std::uint8_t> serialize() const;
-        [[nodiscard]] Eigen::VectorXf compute(Eigen::VectorXf input) const;
 
         class NeuralNetworkDiff {
             std::vector<Eigen::MatrixXf> _weight_diffs;
@@ -47,11 +33,39 @@ namespace lc {
             friend class NeuralNetwork;
         };
 
+        private:
+
+        std::size_t _iterations;
+        std::vector<std::size_t> _layer_sizes;
+
+        std::vector<Eigen::MatrixXf> _weights;
+        std::vector<Eigen::VectorXf> _biases;
+
+        NeuralNetworkDiff _most_recent_diff;
+
+        public:
+
+        explicit NeuralNetwork(std::vector<std::size_t> layer_sizes, bool randomize = true);
+        explicit NeuralNetwork(std::vector<std::uint8_t> alpaca_bytes);
+
         void modify(NeuralNetworkDiff diff, bool apply_biases = true, bool apply_weights = true);
+
+        void train(float cost);
+
+        [[nodiscard]] std::vector<std::uint8_t> serialize() const;
+        [[nodiscard]] Eigen::VectorXf compute(Eigen::VectorXf input) const;
+
+        [[nodiscard]] std::size_t iterations() const;
+        [[nodiscard]] std::size_t layer_count() const;
+        [[nodiscard]] std::size_t layer_size(std::size_t layer) const;
+
+        [[nodiscard]] const Eigen::MatrixXf& weights(std::size_t layer) const;
+        [[nodiscard]] const Eigen::VectorXf& biases(std::size_t layer) const;
+
+        [[nodiscard]] const NeuralNetworkDiff& most_recent_diff() const;
 
         static float sigmoid_abs(float value);
     };
-
 } // namespace lc
 
 #endif // LEXICRAFT_NEURAL_NETWORK_HPP
