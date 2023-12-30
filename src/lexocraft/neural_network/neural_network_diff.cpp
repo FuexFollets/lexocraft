831bc0365e5f4cf0e93a93f7a4872627ff5e882c
FuexFollets <fuex9follets@gmail.com>
2023-12-30 16:44:05 -0500

    Fixed Neuralnetwork::NeuralNetworkDiff oeprator paramters and implemented getters

--- src/lexocraft/neural_network/neural_network_diff.cpp
+++ src/lexocraft/neural_network/neural_network_diff.cpp
@@ -15,7 +15,7 @@ namespace lc {
     }
 
     NeuralNetwork::NeuralNetworkDiff& NeuralNetwork::NeuralNetworkDiff::operator+=(
-        const NeuralNetwork::NeuralNetworkDiff& other) {
+        const NeuralNetwork::NeuralNetworkDiff& other) noexcept {
         for (std::size_t index {0}; index < _layer_sizes.size() - 1; index++) {
             _weight_diffs [index] += other._weight_diffs [index];
             _bias_diffs [index] += other._bias_diffs [index];
@@ -24,7 +24,7 @@ namespace lc {
     }
 
     NeuralNetwork::NeuralNetworkDiff& NeuralNetwork::NeuralNetworkDiff::operator-=(
-        const NeuralNetwork::NeuralNetworkDiff& other) {
+        const NeuralNetwork::NeuralNetworkDiff& other) noexcept {
         for (std::size_t index {0}; index < _layer_sizes.size() - 1; index++) {
             _weight_diffs [index] -= other._weight_diffs [index];
             _bias_diffs [index] -= other._bias_diffs [index];
@@ -32,7 +32,8 @@ namespace lc {
         return *this;
     }
 
-    NeuralNetwork::NeuralNetworkDiff& NeuralNetwork::NeuralNetworkDiff::operator*=(float scalar) {
+    NeuralNetwork::NeuralNetworkDiff&
+        NeuralNetwork::NeuralNetworkDiff::operator*=(float scalar) noexcept {
         for (std::size_t index {0}; index < _layer_sizes.size() - 1; index++) {
             _weight_diffs [index] *= scalar;
             _bias_diffs [index] *= scalar;
@@ -49,7 +50,7 @@ namespace lc {
     }
 
     NeuralNetwork::NeuralNetworkDiff NeuralNetwork::NeuralNetworkDiff::operator+(
-        const NeuralNetwork::NeuralNetworkDiff& other) const {
+        const NeuralNetwork::NeuralNetworkDiff& other) const noexcept {
         NeuralNetwork::NeuralNetworkDiff copy_of_this {*this};
 
         copy_of_this += other;
@@ -58,7 +59,7 @@ namespace lc {
     }
 
     NeuralNetwork::NeuralNetworkDiff NeuralNetwork::NeuralNetworkDiff::operator-(
-        const NeuralNetwork::NeuralNetworkDiff& other) const {
+        const NeuralNetwork::NeuralNetworkDiff& other) const noexcept {
         NeuralNetwork::NeuralNetworkDiff copy_of_this {*this};
 
         copy_of_this -= other;
@@ -67,7 +68,7 @@ namespace lc {
     }
 
     NeuralNetwork::NeuralNetworkDiff
-        NeuralNetwork::NeuralNetworkDiff::operator*(float scalar) const {
+        NeuralNetwork::NeuralNetworkDiff::operator*(float scalar) const noexcept {
         NeuralNetwork::NeuralNetworkDiff copy_of_this {*this};
 
         copy_of_this *= scalar;
@@ -82,5 +83,40 @@ namespace lc {
         copy_of_this /= scalar;
 
         return copy_of_this;
+    }
+
+    /* Implement:
+        [[nodiscard]] std::size_t layer_count() const;
+        [[nodiscard]] std::size_t layer_size(std::size_t layer) const;
+
+        [[nodiscard]] const Eigen::MatrixXf& weight_diff(std::size_t layer) const;
+        [[nodiscard]] const Eigen::VectorXf& bias_diff(std::size_t layer) const;
+
+        [[nodiscard]] Eigen::MatrixXf& weight_diff(std::size_t layer);
+        [[nodiscard]] Eigen::VectorXf& bias_diff(std::size_t layer);
+    */
+
+    std::size_t NeuralNetwork::NeuralNetworkDiff::layer_count() const {
+        return _layer_sizes.size() - 1;
+    }
+
+    std::size_t NeuralNetwork::NeuralNetworkDiff::layer_size(std::size_t layer) const {
+        return _layer_sizes [layer];
+    }
+
+    const Eigen::MatrixXf& NeuralNetwork::NeuralNetworkDiff::weight_diff(std::size_t layer) const {
+        return _weight_diffs [layer];
+    }
+
+    const Eigen::VectorXf& NeuralNetwork::NeuralNetworkDiff::bias_diff(std::size_t layer) const {
+        return _bias_diffs [layer];
+    }
+
+    Eigen::MatrixXf& NeuralNetwork::NeuralNetworkDiff::weight_diff(std::size_t layer) {
+        return _weight_diffs [layer];
+    }
+
+    Eigen::VectorXf& NeuralNetwork::NeuralNetworkDiff::bias_diff(std::size_t layer) {
+        return _bias_diffs [layer];
     }
 } // namespace lc
