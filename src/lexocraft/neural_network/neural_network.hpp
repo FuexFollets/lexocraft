95033ae32fc0456740495d67b2b13fd33a7264fa
FuexFollets <fuex9follets@gmail.com>
2023-12-30 16:22:59 -0500

    Added additional getter and setter headers. Added fields and methods for training.

--- src/lexocraft/neural_network/neural_network.hpp
+++ src/lexocraft/neural_network/neural_network.hpp
@@ -18,22 +18,43 @@ namespace lc {
 
             public:
 
+            NeuralNetworkDiff() = default;
+            NeuralNetworkDiff(NeuralNetworkDiff&& other) noexcept = default;
+            NeuralNetworkDiff(const NeuralNetworkDiff& other) noexcept = default;
             explicit NeuralNetworkDiff(const std::vector<std::size_t>& layer_sizes);
 
-            NeuralNetworkDiff& operator+=(const NeuralNetworkDiff& other);
-            NeuralNetworkDiff& operator-=(const NeuralNetworkDiff& other);
-            NeuralNetworkDiff& operator*=(float scalar);
+            NeuralNetworkDiff& operator=(NeuralNetworkDiff&& other) noexcept = default;
+            NeuralNetworkDiff& operator=(const NeuralNetworkDiff& other) noexcept = default;
+
+            NeuralNetworkDiff& operator+=(const NeuralNetworkDiff& other) noexcept;
+            NeuralNetworkDiff& operator-=(const NeuralNetworkDiff& other) noexcept;
+            NeuralNetworkDiff& operator*=(float scalar) noexcept;
             NeuralNetworkDiff& operator/=(float scalar);
 
-            NeuralNetworkDiff operator+(const NeuralNetworkDiff& other) const;
-            NeuralNetworkDiff operator-(const NeuralNetworkDiff& other) const;
-            NeuralNetworkDiff operator*(float scalar) const;
+            NeuralNetworkDiff operator+(const NeuralNetworkDiff& other) const noexcept;
+            NeuralNetworkDiff operator-(const NeuralNetworkDiff& other) const noexcept;
+            NeuralNetworkDiff operator*(float scalar) const noexcept;
             NeuralNetworkDiff operator/(float scalar) const;
+
+            void invert();
+
+            [[nodiscard]] NeuralNetworkDiff inverted() const;
+
+            [[nodiscard]] std::size_t layer_count() const;
+            [[nodiscard]] std::size_t layer_size(std::size_t layer) const;
+
+            [[nodiscard]] const Eigen::MatrixXf& weight_diff(std::size_t layer) const;
+            [[nodiscard]] const Eigen::VectorXf& bias_diff(std::size_t layer) const;
+
+            [[nodiscard]] Eigen::MatrixXf& weight_diff(std::size_t layer);
+            [[nodiscard]] Eigen::VectorXf& bias_diff(std::size_t layer);
 
             friend class NeuralNetwork;
         };
 
         private:
+
+        constexpr static float GOOD_COST {0.1F};
 
         std::size_t _iterations;
         std::vector<std::size_t> _layer_sizes;
@@ -42,6 +63,8 @@ namespace lc {
         std::vector<Eigen::VectorXf> _biases;
 
         NeuralNetworkDiff _most_recent_diff;
+        float _most_recent_cost;
+        std::size_t _diff_improvement_streak;
 
         public:
 
@@ -63,6 +86,8 @@ namespace lc {
         [[nodiscard]] const Eigen::VectorXf& biases(std::size_t layer) const;
 
         [[nodiscard]] const NeuralNetworkDiff& most_recent_diff() const;
+        [[nodiscard]] float most_recent_cost() const;
+        [[nodiscard]] std::size_t diff_improvement_streak() const;
 
         static float sigmoid_abs(float value);
     };
