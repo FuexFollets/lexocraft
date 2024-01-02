40fbce9d8e52cc36dd8d507a71c8eee5ef8467cb
FuexFollets <fuex9follets@gmail.com>
2024-01-02 15:34:21 -0500

    Removed serialization implementations from NeuralNetwork

--- src/lexocraft/neural_network/neural_network.cpp
+++ src/lexocraft/neural_network/neural_network.cpp
@@ -32,89 +32,6 @@ namespace lc {
         }
     }
 
-    NeuralNetwork::SerializeMedium NeuralNetwork::serialize_medium() const noexcept {
-        using Medium_t = NeuralNetwork::SerializeMedium;
-
-        Medium_t medium;
-
-        medium.iterations = iterations;
-        medium.layer_sizes = layer_sizes;
-        medium.most_recent_diff = most_recent_diff.serialize();
-        medium.most_recent_cost = most_recent_cost;
-        medium.diff_improvement_streak = diff_improvement_streak;
-
-        Eigen::Serializer<Eigen::MatrixXf> matrix_dynamic_serializer;
-        Eigen::Serializer<Eigen::VectorXf> vector_dynamic_serializer;
-
-        medium.weights_buffer.resize(weights.size());
-        medium.biases_buffer.resize(biases.size());
-
-        for (std::size_t index {0}; index < weights.size(); index++) {
-            const auto& weight {weights [index]};
-            const auto size = matrix_dynamic_serializer.size(weight);
-            std::vector<std::uint8_t> bytes(size);
-            auto* buffer = bytes.data();
-            matrix_dynamic_serializer.serialize(buffer, std::next(buffer, size), weight);
-            medium.weights_buffer [index] = std::move(bytes);
-        }
-
-        for (std::size_t index {0}; index < biases.size(); index++) {
-            const auto& bias {biases [index]};
-            const auto size = vector_dynamic_serializer.size(bias);
-            std::vector<std::uint8_t> bytes(size);
-            auto* buffer = bytes.data();
-            vector_dynamic_serializer.serialize(buffer, std::next(buffer, size), bias);
-            medium.biases_buffer [index] = std::move(bytes);
-        }
-
-        return medium;
-    }
-
-    vbuffer_t NeuralNetwork::serialize() const noexcept {
-        using Medium_t = NeuralNetwork::SerializeMedium;
-
-        Medium_t medium {serialize_medium()};
-
-        vbuffer_t buffer;
-
-        alpaca::serialize<Medium_t>(medium, buffer);
-
-        return buffer;
-    }
-
-    NeuralNetwork NeuralNetwork::SerializeMedium::demediumize() const noexcept {
-        // Go from this medium to a neural network
-
-        NeuralNetwork network;
-
-        network.iterations = iterations;
-        network.layer_sizes = layer_sizes;
-        network.most_recent_diff = most_recent_diff.demediumize();
-        network.most_recent_cost = most_recent_cost;
-        network.diff_improvement_streak = diff_improvement_streak;
-
-        Eigen::Serializer<Eigen::MatrixXf> matrix_dynamic_deserializer;
-        Eigen::Serializer<Eigen::VectorXf> vector_dynamic_deserializer;
-
-        for (std::size_t index {0}; index < weights_buffer.size(); index++) {
-            const auto& bytes = weights_buffer [index];
-            const auto size = bytes.size();
-            const auto* buffer = bytes.data();
-            matrix_dynamic_deserializer.deserialize(buffer, std::next(buffer, size),
-                                                    network.weights [index]);
-        }
-
-        for (std::size_t index {0}; index < biases_buffer.size(); index++) {
-            const auto& bytes = biases_buffer [index];
-            const auto size = bytes.size();
-            const auto* buffer = bytes.data();
-            vector_dynamic_deserializer.deserialize(buffer, std::next(buffer, size),
-                                                    network.biases [index]);
-        }
-
-        return network;
-    }
-
     Eigen::VectorXf NeuralNetwork::compute(Eigen::VectorXf input) const noexcept {
         for (std::size_t index {0}; index < weights.size(); ++index) {
             input = weights [index] * input + biases [index];
@@ -179,21 +96,6 @@ namespace lc {
             ++diff_improvement_streak;
             most_recent_diff *= 1.0F + diff_improvement_streak / 10.0F;
         }
-    }
-
-    void NeuralNetwork::dump_file(const std::filesystem::path& filepath) const {
-        std::ofstream dumpfile(filepath, std::ios::out | std::ios::binary);
-
-        alpaca::serialize<NeuralNetwork::SerializeMedium>(serialize_medium(), dumpfile);
-    }
-
-    NeuralNetwork NeuralNetwork::load_file(const std::filesystem::path& filepath) {
-        std::ifstream dumpfile(filepath, std::ios::in | std::ios::binary);
-        std::error_code error_code;
-        const auto file_size = std::filesystem::file_size(filepath);
-
-        return alpaca::deserialize<NeuralNetwork::SerializeMedium>(dumpfile, file_size, error_code)
-            .demediumize();
     }
 
 } // namespace lc
