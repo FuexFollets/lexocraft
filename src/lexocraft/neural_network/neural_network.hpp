c699e5b6c4a2341192d224b36e9c43aa6ef4f4ec
FuexFollets <fuex9follets@gmail.com>
2024-01-02 15:33:17 -0500

    Removed current methods of serialization from headers

--- src/lexocraft/neural_network/neural_network.hpp
+++ src/lexocraft/neural_network/neural_network.hpp
@@ -17,16 +17,6 @@ namespace lc {
         class NeuralNetworkDiff {
             public:
 
-            class SerializeMedium {
-                public:
-
-                std::vector<vbuffer_t> weight_diffs_buffers;
-                std::vector<vbuffer_t> bias_diffs_buffers;
-                std::vector<std::size_t> layer_sizes;
-
-                [[nodiscard]] NeuralNetworkDiff demediumize() const noexcept;
-            };
-
             constexpr static std::size_t FIELD_COUNT {3};
             std::vector<Eigen::MatrixXf> weight_diffs;
             std::vector<Eigen::VectorXf> bias_diffs;
@@ -53,25 +43,8 @@ namespace lc {
             void invert() noexcept;
 
             [[nodiscard]] NeuralNetworkDiff inverted() const noexcept;
-            [[nodiscard]] SerializeMedium serialize() const noexcept;
 
             friend class NeuralNetwork;
-        };
-
-        class SerializeMedium {
-            public:
-
-            std::size_t iterations {};
-            std::vector<std::size_t> layer_sizes;
-
-            std::vector<vbuffer_t> weights_buffer;
-            std::vector<vbuffer_t> biases_buffer;
-
-            NeuralNetworkDiff::SerializeMedium most_recent_diff;
-            float most_recent_cost {};
-            std::size_t diff_improvement_streak {};
-
-            [[nodiscard]] NeuralNetwork demediumize() const noexcept;
         };
 
         constexpr static float GOOD_COST {0.1F};
@@ -99,8 +72,6 @@ namespace lc {
 
         void train(float cost);
 
-        [[nodiscard]] SerializeMedium serialize_medium() const noexcept;
-        [[nodiscard]] vbuffer_t serialize() const noexcept;
         [[nodiscard]] Eigen::VectorXf compute(Eigen::VectorXf input) const noexcept;
 
         void dump_file(const std::filesystem::path& filepath) const;
