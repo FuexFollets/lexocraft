889694547bce666099a2c4274275de243b817962
FuexFollets <fuex9follets@gmail.com>
2024-01-03 10:49:18 -0500

    Tested neural network serialization

--- tests/neural_network_serialization.cpp
+++ tests/neural_network_serialization.cpp
@@ -1,3 +1,4 @@
+#include <filesystem>
 #include <iostream>
 #include <lexocraft/neural_network/neural_network.hpp>
 #include <string>
@@ -6,37 +7,28 @@
 int main(int argc, char** argv) {
     // Test neural network serialization
 
-    std::vector<std::size_t> layer_sizes {10, 7, 7, 10};
-    lc::NeuralNetwork network(layer_sizes);
+    // print arguments
 
-    std::cout << "Neural network created:\n" << network << "\n";
-    std::cout << "Serialized and deserialized neural network:\n";
+    std::cout << "argc: " << argc << "\n";
+    for (int index = 0; index < argc; ++index) {
+        std::cout << "argv[" << index << "]: " << argv [index] << "\n";
+    }
 
-    /*
-    const std::string filename {*std::next(argv, 1)};
-
-    std::cout << "Filename: " << filename << '\n';
+    std::filesystem::path path = *std::next(argv, 1);
 
     if (argc > 2) {
-        // save at filename
         std::vector<std::size_t> layer_sizes {10, 7, 7, 10};
         lc::NeuralNetwork network(layer_sizes);
 
         std::cout << "Neural network created:\n" << network << "\n";
+        std::cout << "Serialized and deserialized neural network:\n";
+        network.save_file(path);
 
-        network.dump_file(filename);
-
-        std::cout << "Neural network saved\n";
+        std::cout << "Network saved to file: " << path << "\n";
     }
-
     else {
-        // load from filename
-
-        lc::NeuralNetwork network = lc::NeuralNetwork::load_file(filename);
-
-        std::cout << "Neural network loaded\n";
-
-        std::cout << "Neural network:\n" << network << "\n";
+        lc::NeuralNetwork loaded_network = lc::NeuralNetwork::load_file(path);
+        std::cout << "Loaded file from path: " << path << "\n";
+        std::cout << "Loaded neural network:\n" << loaded_network << "\n";
     }
-    */
 }
