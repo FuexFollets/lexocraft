3f55a6c9e5a234ebc48a13d86b46d624ef77f8ea
FuexFollets <fuex9follets@gmail.com>
2024-01-06 15:48:27 -0500

    Added word vector database header

--- src/lexocraft/llm/vector_database.hpp
+++ src/lexocraft/llm/vector_database.hpp
@@ -1,8 +1,46 @@
 #ifndef LEXOCRAFT_VECTOR_DATABASE_HPP
 #define LEXOCRAFT_VECTOR_DATABASE_HPP
 
-namespace lc {
+#include <filesystem>
+#include <string>
+#include <vector>
 
-}
+#include <Eigen/Eigen>
+
+#include <lexocraft/cereal_eigen.hpp>
+
+namespace lc {
+    class WordVector {
+        public:
+
+        static constexpr std::size_t WORD_VECTOR_DIMENSIONS = 32;
+
+        std::string word;
+        Eigen::Vector<float, WORD_VECTOR_DIMENSIONS> vector;
+
+        template <class Archive>
+        void serialize(Archive& archive) {
+            archive(word, vector);
+        }
+    };
+
+    class VectorDatabase {
+        public:
+
+        VectorDatabase() = default;
+        explicit VectorDatabase(const std::vector<WordVector>& words);
+        explicit VectorDatabase(const std::filesystem::path& filepath);
+
+        std::vector<WordVector> words;
+
+        void save(const std::filesystem::path& filepath) const;
+        void load(const std::filesystem::path& filepath);
+
+        template <class Archive>
+        void serialize(Archive& archive) {
+            archive(words);
+        }
+    };
+} // namespace lc
 
 #endif // LEXOCRAFT_VECTOR_DATABASE_HPP
