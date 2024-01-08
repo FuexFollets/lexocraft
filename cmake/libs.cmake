8c5e5f626e3d4bd5a2bef5d5c7d7b8c43b250096
FuexFollets <fuex9follets@gmail.com>
2024-01-08 15:19:55 -0500

    Added rapidfuzz to cmake and libraries

--- cmake/libs.cmake
+++ cmake/libs.cmake
@@ -1,9 +1,10 @@
 include_directories(${PROJECT_SOURCE_DIR}/lib/eigen)
 include_directories(${PROJECT_SOURCE_DIR}/lib/cereal/include)
 include_directories(${PROJECT_SOURCE_DIR}/lib/eternal/include)
+include_directories(${PROJECT_SOURCE_DIR}/lib/rapidfuzz-cpp)
 include_directories(${PROJECT_SOURCE_DIR}/src)
 
-set(LEXOCRAFT_LIBS neural_network llm)
+set(LEXOCRAFT_LIBS neural_network llm rapidfuzz::rapidfuzz)
 
 # include(${PROJECT_SOURCE_DIR}/cmake/libs.cmake)
 # include(${PROJECT_SOURCE_DIR}/cmake/opts.cmake)
