b1a0ce024ad040a51f19a2db37e8d32c78998611
FuexFollets <fuex9follets@gmail.com>
2024-01-10 13:38:56 -0500

    Added member variables to text completion class header. Added neural network vector fields structure

--- src/lexocraft/llm/text_completion.hpp
+++ src/lexocraft/llm/text_completion.hpp
@@ -1,8 +1,39 @@
 #ifndef TEXT_COMPLETION_HPP
 #define TEXT_COMPLETION_HPP
 
+#include <Eigen/Eigen>
+
+#include <lexocraft/llm/vector_database.hpp>
 #include <lexocraft/neural_network/neural_network.hpp>
 
-class TextCompleter {};
+namespace lc {
+    class TextCompleter {
+        public:
+
+        Eigen::VectorXf ephemeral_memory;
+        Eigen::VectorXf context_memory;
+
+        NeuralNetwork ephemeral_memory_accmulator;
+        NeuralNetwork context_builder;
+        VectorDatabase* vector_database;
+
+        struct EphemeralMemoryNNFields {
+            /* TODO: Vector fields for EphemeralMemoryNN */
+        };
+
+        struct EphemeralMemoryNNOutput {
+            /* TODO: Vector fields for EphemeralMemoryNN output */
+        };
+
+        struct ContextBuilderNNFields {
+            /* TODO: Vector fields for ContextBuilderNN */
+        };
+
+        struct ContextBuilderNNOutput {
+            /* TODO: Vector fields for ContextBuilderNN output */
+        };
+    };
+
+} // namespace lc
 
 #endif // TEXT_COMPLETION_HPP
