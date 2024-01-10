3b6b16d1c213ecde8c3b0444eeb6e35e73e987c8
FuexFollets <fuex9follets@gmail.com>
2024-01-10 09:27:28 -0500

    Added benchmarking with iterations option and hash map serach

--- tests/vector_database_search.cpp
+++ tests/vector_database_search.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <ios>
 #include <iostream>
 #include <string>
@@ -15,8 +16,9 @@ int main(int argc, char** argv) {
 
     if (args.size() < 2) {
         std::cout
-            << "Usage: <database> <searched_word> [rapidfuzz/default] OPTIONAL: <top_n> "
-               "<threshold> <soundex_weight> <levenshtein_weight> <stop_when_top_n_are_found>";
+            << "Usage: <database> <searched_word> [rapidfuzz/default/hash] <iterations> OPTIONAL: "
+               "<top_n> <threshold> <soundex_weight> <levenshtein_weight> "
+               "<stop_when_top_n_are_found>";
 
         return 1;
     }
@@ -24,25 +26,31 @@ int main(int argc, char** argv) {
     const std::string database_path = args.at(0);
     const std::string searched_word = args.at(1);
     const bool use_rapidfuzz = args.at(2) == "rapidfuzz";
+    const bool use_hash = args.at(2) == "hash";
+    const std::size_t iterations = std::stoi(args.at(3));
 
     const bool optional_args_provided = args.size() > 3;
 
-    const int top_n = optional_args_provided ? std::stoi(args.at(3)) : 10;
-    const float threshold = optional_args_provided ? std::stof(args.at(4)) : 0.2F;
-    const float soundex_weight = optional_args_provided ? std::stof(args.at(5)) : 0.5F;
-    const float levenshtein_weight = optional_args_provided ? std::stof(args.at(6)) : 0.5F;
+    const int top_n = optional_args_provided ? std::stoi(args.at(4)) : 10;
+    const float threshold = optional_args_provided ? std::stof(args.at(5)) : 0.2F;
+    const float soundex_weight = optional_args_provided ? std::stof(args.at(6)) : 0.5F;
+    const float levenshtein_weight = optional_args_provided ? std::stof(args.at(7)) : 0.5F;
     const bool stop_when_top_n_are_found =
-        optional_args_provided ? static_cast<bool>(std::stoi(args.at(7))) : false;
+        optional_args_provided ? static_cast<bool>(std::stoi(args.at(8))) : false;
 
     std::cout << std::boolalpha;
 
-    std::cout << "database_path: " << database_path << "\n"
-              << "searched_word: " << searched_word << "\n"
-              << "top_n: " << top_n << "\n"
-              << "threshold: " << threshold << "\n"
-              << "soundex_weight: " << soundex_weight << "\n"
-              << "levenshtein_weight: " << levenshtein_weight << "\n"
-              << "stop_when_top_n_are_found: " << stop_when_top_n_are_found << "\n";
+    if (!use_hash) {
+        std::cout << "database_path: " << database_path << "\n"
+                  << "searched_word: " << searched_word << "\n"
+                  << "method: " << (use_rapidfuzz ? "rapidfuzz" : "default") << "\n"
+                  << "iterations: " << iterations << "\n"
+                  << "top_n: " << top_n << "\n"
+                  << "threshold: " << threshold << "\n"
+                  << "soundex_weight: " << soundex_weight << "\n"
+                  << "levenshtein_weight: " << levenshtein_weight << "\n"
+                  << "stop_when_top_n_are_found: " << stop_when_top_n_are_found << "\n";
+    }
 
     lc::VectorDatabase database;
 
@@ -54,12 +62,62 @@ int main(int argc, char** argv) {
 
     std::cout << "Searching for " << searched_word << "...\n";
 
+    if (use_hash) {
+        // measure time
+
+        std::size_t average_duration_ns = 0;
+        const auto result = database.search_from_map(searched_word);
+
+        for (std::size_t iteration {0}; iteration < iterations; ++iteration) {
+            const auto start = std::chrono::high_resolution_clock::now();
+            const auto result = database.search_from_map(searched_word);
+            const auto end = std::chrono::high_resolution_clock::now();
+            const auto duration_ns =
+                std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+            average_duration_ns += duration_ns.count();
+        }
+
+        average_duration_ns /= iterations;
+
+        std::cout << "Average duration: " << average_duration_ns << " nanoseconds = ("
+                  << average_duration_ns / 1000000.0F << " ms)\n";
+
+        if (result.has_value()) {
+            std::cout << "Search result: " << result->word << "\n";
+        }
+
+        else {
+            std::cout << "Search result: not found\n";
+        }
+
+        return 0;
+    }
+
+    std::size_t average_duration_ns = 0;
+
     const auto results =
-        use_rapidfuzz
-            ? database.rapidfuzz_search_closest_n(searched_word, top_n, threshold,
-                                                  stop_when_top_n_are_found)
-            : database.search_closest_n(searched_word, top_n, threshold, soundex_weight,
-                                        levenshtein_weight, stop_when_top_n_are_found);
+        use_rapidfuzz ? database.rapidfuzz_search_closest_n(searched_word, top_n, threshold,
+                                                            stop_when_top_n_are_found)
+                      : database.search_closest_n(searched_word, top_n, threshold, soundex_weight,
+                                                  levenshtein_weight, stop_when_top_n_are_found);
+
+    for (std::size_t iteration {0}; iteration < iterations; ++iteration) {
+        const auto start = std::chrono::high_resolution_clock::now();
+        const auto results =
+            use_rapidfuzz
+                ? database.rapidfuzz_search_closest_n(searched_word, top_n, threshold,
+                                                      stop_when_top_n_are_found)
+                : database.search_closest_n(searched_word, top_n, threshold, soundex_weight,
+                                            levenshtein_weight, stop_when_top_n_are_found);
+        const auto end = std::chrono::high_resolution_clock::now();
+        const auto duration_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+        average_duration_ns += duration_ns.count();
+    }
+
+    average_duration_ns /= iterations;
+
+    std::cout << "Average duration: " << average_duration_ns << " nanoseconds = ("
+              << average_duration_ns / 1000000.0F << " ms)\n";
 
     std::cout << "Search results:\n";
 
