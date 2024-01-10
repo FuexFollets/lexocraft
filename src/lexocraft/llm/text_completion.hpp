6e379dc88a4c919313cb8f8fb1b790a1d4e86785
FuexFollets <fuex9follets@gmail.com>
2024-01-10 13:54:44 -0500

    Added virtual methods for output and input vector fields

--- src/lexocraft/llm/text_completion.hpp
+++ src/lexocraft/llm/text_completion.hpp
@@ -17,19 +17,30 @@ namespace lc {
         NeuralNetwork context_builder;
         VectorDatabase* vector_database;
 
-        struct EphemeralMemoryNNFields {
+        struct NNFieldsInput {
+            virtual ~NNFieldsInput() = default; // Abstract
+            [[nodiscard]] virtual Eigen::VectorXf to_vector() const = 0;
+        };
+
+        template <typename Output>
+        struct NNOutput { // Abstract
+            virtual ~NNOutput() = default;
+            [[nodiscard]] virtual Output from_vector(Eigen::VectorXf) const = 0;
+        };
+
+        struct EphemeralMemoryNNFields : NNFieldsInput {
             /* TODO: Vector fields for EphemeralMemoryNN */
         };
 
-        struct EphemeralMemoryNNOutput {
+        struct EphemeralMemoryNNOutput : NNOutput<EphemeralMemoryNNOutput> {
             /* TODO: Vector fields for EphemeralMemoryNN output */
         };
 
-        struct ContextBuilderNNFields {
+        struct ContextBuilderNNFields : NNFieldsInput {
             /* TODO: Vector fields for ContextBuilderNN */
         };
 
-        struct ContextBuilderNNOutput {
+        struct ContextBuilderNNOutput : NNOutput<ContextBuilderNNOutput> {
             /* TODO: Vector fields for ContextBuilderNN output */
         };
     };
