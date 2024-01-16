ca9e9285e11fc0e69b15b348cd6f56ce0d206682
FuexFollets <fuex9follets@gmail.com>
2024-01-16 12:10:57 -0500

    Renamed cmake libraries to avoid name collisions

--- cmake/libs.cmake
+++ cmake/libs.cmake
@@ -5,7 +5,7 @@ include_directories(${PROJECT_SOURCE_DIR}/lib/rapidfuzz-cpp)
 include_directories(${PROJECT_SOURCE_DIR}/lib/robin-map/include)
 include_directories(${PROJECT_SOURCE_DIR}/src)
 
-set(LEXOCRAFT_LIBS neural_network llm rapidfuzz::rapidfuzz tsl::robin_map)
+set(LEXOCRAFT_LIBS lexocraft_neural_network lexocraft_llm rapidfuzz::rapidfuzz tsl::robin_map)
 
 # include(${PROJECT_SOURCE_DIR}/cmake/libs.cmake)
 # include(${PROJECT_SOURCE_DIR}/cmake/opts.cmake)
