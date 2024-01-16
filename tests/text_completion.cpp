c7de68c8d86bce5f2c289add89b2e20f9e5cd15c
FuexFollets <fuex9follets@gmail.com>
2024-01-16 12:04:56 -0500

    Tested TextCompleter constructor

--- tests/text_completion.cpp
+++ tests/text_completion.cpp
@@ -2,6 +2,7 @@
 
 #include <lexocraft/llm/text_completion.hpp>
 #include <lexocraft/llm/vector_database.hpp>
+#include <memory>
 
 int main(int argc, char** argv) {
     std::vector<std::string> args {std::next(argv, 1), std::next(argv, argc)};
@@ -11,4 +12,19 @@ int main(int argc, char** argv) {
     for (const auto& arg: args) {
         std::cout << "Arg: " << arg << "\n";
     }
+
+    const std::string database_path = args.at(0);
+
+    std::cout << "database_path: " << database_path << "\n";
+
+    lc::VectorDatabase database {};
+
+    database.load(database_path);
+
+    std::cout << "Database loaded from path: " << database_path << "\n";
+    std::cout << "Constructing text completer\n";
+
+    lc::TextCompleter completer {std::make_shared<lc::VectorDatabase>(database), 500, 500};
+
+    std::cout << "Text completer created\n";
 }
