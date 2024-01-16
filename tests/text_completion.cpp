aa4f73fdd7a531716faea1037cfd62a665ffa21a
FuexFollets <fuex9follets@gmail.com>
2024-01-16 10:51:20 -0500

    Implemented TextCompleter Neural Network and Vector Database setters with ascertatin checks

--- tests/text_completion.cpp
+++ tests/text_completion.cpp
@@ -1,1 +1,14 @@
-int main() {}
+#include <iostream>
+
+#include <lexocraft/llm/text_completion.hpp>
+#include <lexocraft/llm/vector_database.hpp>
+
+int main(int argc, char** argv) {
+    std::vector<std::string> args {std::next(argv, 1), std::next(argv, argc)};
+
+    std::cout << "args: " << args.size() << "\n";
+
+    for (const auto& arg: args) {
+        std::cout << "Arg: " << arg << "\n";
+    }
+}
