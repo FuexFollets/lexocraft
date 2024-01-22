57cc9b956053f23e857e33c0b475a8cced538e19
FuexFollets <fuex9follets@gmail.com>
2024-01-22 11:09:17 -0500

    Added google/benchmark library

--- cmake/libs.cmake
+++ cmake/libs.cmake
@@ -3,9 +3,10 @@ include_directories(${PROJECT_SOURCE_DIR}/lib/cereal/include)
 include_directories(${PROJECT_SOURCE_DIR}/lib/eternal/include)
 include_directories(${PROJECT_SOURCE_DIR}/lib/rapidfuzz-cpp)
 include_directories(${PROJECT_SOURCE_DIR}/lib/robin-map/include)
+include_directories(${PROJECT_SOURCE_DIR}/lib/benchmark/include)
 include_directories(${PROJECT_SOURCE_DIR}/src)
 
-set(LEXOCRAFT_LIBS lexocraft_neural_network lexocraft_llm rapidfuzz::rapidfuzz tsl::robin_map)
+set(LEXOCRAFT_LIBS lexocraft_neural_network lexocraft_llm rapidfuzz::rapidfuzz tsl::robin_map benchmark::benchmark)
 
 # include(${PROJECT_SOURCE_DIR}/cmake/libs.cmake)
 # include(${PROJECT_SOURCE_DIR}/cmake/opts.cmake)
