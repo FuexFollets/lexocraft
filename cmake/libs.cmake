552443df391d7fbe3742fdb7ed8b19f65de30655
FuexFollets <fuex9follets@gmail.com>
2024-01-23 14:06:30 -0500

    Added option for google benchmark

--- cmake/libs.cmake
+++ cmake/libs.cmake
@@ -3,10 +3,18 @@ include_directories(${PROJECT_SOURCE_DIR}/lib/cereal/include)
 include_directories(${PROJECT_SOURCE_DIR}/lib/eternal/include)
 include_directories(${PROJECT_SOURCE_DIR}/lib/rapidfuzz-cpp)
 include_directories(${PROJECT_SOURCE_DIR}/lib/robin-map/include)
-include_directories(${PROJECT_SOURCE_DIR}/lib/benchmark/include)
+include_directories(${PROJECT_SOURCE_DIR}/lib/annoy/src)
 include_directories(${PROJECT_SOURCE_DIR}/src)
 
-set(LEXOCRAFT_LIBS lexocraft_neural_network lexocraft_llm rapidfuzz::rapidfuzz tsl::robin_map benchmark::benchmark)
+if(UseGoogleBenchmark)
+    include_directories(${PROJECT_SOURCE_DIR}/lib/benchmark/include)
+endif()
+
+set(LEXOCRAFT_LIBS lexocraft_neural_network
+    lexocraft_llm
+    rapidfuzz::rapidfuzz
+    tsl::robin_map
+    $<IF:$<BOOL:${USE_GOOGLE_BENCHMARK}>,benchmark::benchmark,>)
 
 # include(${PROJECT_SOURCE_DIR}/cmake/libs.cmake)
 # include(${PROJECT_SOURCE_DIR}/cmake/opts.cmake)
