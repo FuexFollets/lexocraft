e0106fca5754bc4e675238bb9ee1878184fe7dda
Rajin Braynard <85237518+FuexFollets@users.noreply.github.com>
2024-01-27 12:48:49 -0500

    Fixed opts.cmake compile flags based on compiler

--- cmake/opts.cmake
+++ cmake/opts.cmake
@@ -4,10 +4,15 @@ set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 set(CMAKE_CXX_STANDARD 20)
 
-if(CMAKE_COMPILER_IS_GNUCXX)
-    message(STATUS "GCC detected, adding compile flags")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wpedantic -g")
+set(base_flags "-Wall -Wextra -Wpedantic -g")
+
+if (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
+    # Remove -Wextra for MSVC
+    string(REPLACE "-Wextra" "" flags "${base_flags}")
+else()
+    # Use base flags for other compilers
+    set(flags "${base_flags}")
 endif()
 
-set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -g -O0")
-
+# Apply the final flags
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flags}")
