011332a6ce34976670b9202540b9ee3c83dc9f2a
FuexFollets <fuex9follets@gmail.com>
2024-02-04 18:08:02 -0500

    Updated cmake. Made nanobench optional and added cmake dev script

--- cmake/libs.cmake
+++ cmake/libs.cmake
@@ -4,10 +4,14 @@ include_directories(${PROJECT_SOURCE_DIR}/lib/eternal/include)
 include_directories(${PROJECT_SOURCE_DIR}/lib/rapidfuzz-cpp)
 include_directories(${PROJECT_SOURCE_DIR}/lib/robin-map/include)
 include_directories(${PROJECT_SOURCE_DIR}/lib/annoy/include)
+include_directories(${PROJECT_SOURCE_DIR}/lib/nanobench/src/include)
 include_directories(${PROJECT_SOURCE_DIR}/src)
 
+option(USE_NANOBENCH "Use nanobench" OFF)
 
 set(LEXOCRAFT_LIBS lexocraft_neural_network
     lexocraft_llm
     rapidfuzz::rapidfuzz
-    tsl::robin_map)
+    tsl::robin_map
+    $<IF:$<BOOL:${USE_NANOBENCH}>,nanobench,>
+    )
