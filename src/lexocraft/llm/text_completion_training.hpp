a363df3e99975b1fdfd47afd6e68395d060b098b
FuexFollets <fuex9follets@gmail.com>
2024-02-08 15:27:37 -0500

    Added method headers and fields for text completion trainer and text completion training implementation file

--- src/lexocraft/llm/text_completion_training.hpp
+++ src/lexocraft/llm/text_completion_training.hpp
@@ -2,21 +2,31 @@
 #define LEXOCRAFT_TEXT_COMPLETION_TRAINING_HPP
 
 #include <optional>
+#include <string>
 
 #include <lexocraft/llm/text_completion.hpp>
 #include <lexocraft/neural_network/neural_network.hpp>
 
 namespace lc {
-    class TextCompletionTraining {
+    class TextCompletionTrainer {
         public:
 
-        enum class Modifier {
-            EphemeralMemory = 0b0001,
-            ContextBuilder = 0b0010,
-            word_vector_improviser = 0b0100,
+        struct TrainingModification {
+            std::optional<NeuralNetwork::NeuralNetworkDiff> ephemeral_memory_diff;
+            std::optional<NeuralNetwork::NeuralNetworkDiff> context_builder_diff;
+            std::optional<NeuralNetwork::NeuralNetworkDiff> word_vector_improviser_diff;
+
+            float original_cost {};
+            float improved_cost {};
         };
 
         std::optional<TextCompleter> text_completer;
+
+        TrainingModification
+            train_neural_network(const std::vector<std::string>& training_data_sections);
+        TrainingModification train_neural_network(const std::string& training_data);
+
+        TextCompleter& apply_training_modification(const TrainingModification& modification);
     };
 } // namespace lc
 
